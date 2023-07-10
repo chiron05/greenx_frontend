9c0c507cee1cf4a2e525511117f9b28cd2a01950
sarthak <sarthaknaik010@gmail.com>
2023-07-10 17:19:26 +0530

    create product

--- src/components/Product/Createproduct.js
+++ src/components/Product/Createproduct.js
@@ -1,6 +1,19 @@
 import React, { useState } from 'react';
 import './Createproduct.css'
+import { Image } from 'cloudinary-react';
+import { Cloudinary } from 'cloudinary-core';
+
+
+
+
 const imge='../../images/img2.png'
+const cloudinary = new Cloudinary({
+  cloud_name: 'dgi3gedck',
+  api_key: '864638863547999',
+  api_secret: 'tB4PpiFcOVrG2pidsc-pi2JMSys'
+});
+
+
 const Createproduct = () => {
   const [productname, setName] = useState('');
   const [description, setDescription] = useState('');
@@ -8,6 +21,8 @@ const Createproduct = () => {
   const [quantity, setQuantity] = useState('');
   const [pincode, setPincode] = useState('');
   const [images, setImages] = useState([]);
+  // const [selectedFiles, setSelectedFiles] = useState([]);
+  const [uploadError, setUploadError] = useState('');
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -33,12 +48,47 @@ const Createproduct = () => {
     }
   };
 
-  const handleImageUpload = (e) => {
-    const selectedImages = Array.from(e.target.files);
-    setImages(selectedImages);
-  };
+ // Inside your component...
+let imageData =[];
+const handleImageUpload = async (event) => {
+  let allimage=[];
+  try {
+    const files = event.target.files;
 
-  const handleSubmit = (e) => {
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const data = new FormData()
+    data.append("file",file)
+    data.append("upload_preset","greenx")
+    data.append("cloud_name","dyog3kn22")
+    const resposne=await fetch("https://api.cloudinary.com/v1_1/dyog3kn22/image/upload",{
+      method:"post",
+      body:data
+    })
+    const result = await resposne.json();
+    const imageUrl = result.secure_url;
+    allimage[i]=imageUrl
+    }
+    console.log(allimage)
+    let x=0;
+    for(x=0;x<allimage.length;x++){
+      imageData[x]=allimage[x]
+    }
+    console.log(imageData)
+    setUploadError(''); // Clear the upload error
+    console.log("images"+images)
+  } catch (error) {
+        setUploadError('Error uploading images. Please try again.');
+        return;
+  }
+
+  
+};
+
+
+
+
+  const handleSubmit = async(e) => {
     e.preventDefault();
 
     // Here you can perform any necessary validation and submit the form data
@@ -60,20 +110,20 @@ const Createproduct = () => {
           <h2 style={{fontFamily: 'Open Sans, sans-serif' , color:'green'}}>CREATE A PRODUCT</h2>
           <form onSubmit={handleSubmit}>
             <div>
-            <input type="text"  name="productname"  value={productname} onChange={handleInputChange} class="universeInput" placeholder="PRODUCT NAME..." ></input>
+            <input type="text"  name="productname"  value={productname} onChange={handleInputChange} className="universeInput" placeholder="PRODUCT NAME..." ></input>
             </div>
             <div>
-            <textarea type="text"   id="description"  name="description" value={description}  onChange={handleInputChange} class="universeInput" placeholder="DESCRIPTION..."></textarea>
+            <textarea type="text"   id="description"  name="description" value={description}  onChange={handleInputChange} className="universeInput" placeholder="DESCRIPTION..."></textarea>
             </div>
             <div>
-               <input type="number" id="price" name="price"  value={price} onChange={handleInputChange} class="universeInput" placeholder="PRICE..."></input>
+               <input type="number" id="price" name="price"  value={price} onChange={handleInputChange} className="universeInput" placeholder="PRICE..."></input>
             </div>
             <div>
-            <input type="text" id="quantity"  name="quantity"  value={quantity} onChange={handleInputChange} class="universeInput" placeholder="QUANTITY..."></input>
+            <input type="text" id="quantity"  name="quantity"  value={quantity} onChange={handleInputChange} className="universeInput" placeholder="QUANTITY..."></input>
             </div>
             <div>
               <select
-              class="universeInput"
+              className="universeInput"
               id="pincode"
               name="pincode"
               value={pincode}
@@ -89,14 +139,15 @@ const Createproduct = () => {
               <input
               type="file"
               id="images"
-              name="images"
+              name="files"
               accept="image/*"
               multiple
-              class="universeInput"
+              className="universeInput"
               onChange={handleImageUpload}
             />
+            {uploadError && <p style={{color:'red',fontFamily:'Open Sans, sans-serif'}}>{uploadError}</p>}
             </div>
-            <button type="submit">Create</button>
+            <button type="submit">SUBMIT</button>
            
           </form>
         </div>
