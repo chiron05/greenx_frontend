d42f6387a1ce2f33c7ff75b167eb453b9304a8b6
sarthak <sarthaknaik010@gmail.com>
2023-07-12 17:57:03 +0530

    geo searching done

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import MainProducts from './components/MainProducts';
 import SellerHome from './components/Seller/SellerHome';
 import Createproduct from './components/Product/Createproduct';
 import CategorySelection from './components/Product/CategorySelection';
+import Geosearchpage from './components/GeoSearch/Geosearchpage';
 
 
 
@@ -30,6 +31,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/advertisement" component={Advertisement} />
+        <Route path="/geosearchpage" component={Geosearchpage} />
         <Route path="/createproduct" component={Createproduct} />
         <Route path="/CategorySelection" component={CategorySelection} />
         <Route path="/authentication" component={Authentication} />
