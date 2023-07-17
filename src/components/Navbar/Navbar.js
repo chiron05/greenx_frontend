4e49ef5c99ecab90bdef20ade0731f32e8fe6799
Praharsh-pai <dementor1901@gmail.com>
2023-07-17 16:17:52 +0530

    add

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,20 +5,17 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
 
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
 
   return (
     <nav style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', backgroundColor: '#f1f7f1', boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.1)', padding: '10px', position: 'fixed', width: '99vw', zIndex: '10' }}>
       <div style={{ display: 'flex', alignItems: 'center' }}>
         <strong style={{ fontSize: '50px', fontStyle: 'italic', color: "#046930", marginLeft:"30px"}}>GreenX</strong>
       </div>
-      {(isSidebarOpen)? <div>helo</div> : <div></div>}
       <div style={{ display: 'flex', alignItems: 'center' ,marginRight:"50px"}}>
         <Link to='/'  style={{textDecoration: 'none' }}><span style={{ marginRight: '20px', fontFamily: 'Open Sans, sans-serif', fontWeight: 'bold', color: '#546360', cursor: 'pointer', textDecoration: 'none' }}>
           HOME
           </span></Link>
-        {/* <span ><a href='#home' style={{ marginRight: '20px', fontFamily: 'Open Sans, sans-serif', fontWeight: 'bold', color: '#546360', cursor: 'pointer', textDecoration: 'none' }}>
+        <span ><a href='#home' style={{ marginRight: '20px', fontFamily: 'Open Sans, sans-serif', fontWeight: 'bold', color: '#546360', cursor: 'pointer', textDecoration: 'none' }}>
           TRENDING
         </a></span>
         <span ><a href='#search' style={{ marginRight: '20px', fontFamily: 'Open Sans, sans-serif', fontWeight: 'bold', color: '#546360', cursor: 'pointer', textDecoration: 'none' }}>
@@ -32,10 +29,8 @@ function Navbar() {
         </a></span>
         <span ><a href='#products' style={{ marginRight: '20px', fontFamily: 'Open Sans, sans-serif', fontWeight: 'bold', color: '#546360', cursor: 'pointer', textDecoration: 'none' }}>
           POPULAR PRODUCTS
-        </a></span> */}
-        <Link to='/categorypage'  style={{textDecoration: 'none' }}><span style={{ marginRight: '20px', fontFamily: 'Open Sans, sans-serif', fontWeight: 'bold', color: '#546360', cursor: 'pointer', textDecoration: 'none' }}>
-          PRODUCTS
-          </span></Link>
+        </a></span>
+
         <Link to='/authentication'> <button style={{ borderRadius: '5px', border: 'none', backgroundColor: '#2182a1', color: '#2182a1', padding: '10px 20px' }}><div style={{ fontFamily: 'Open Sans, sans-serif', fontWeight: 'bold', color: 'white' }}>LOG IN / SIGN UP</div></button></Link>
       </div>
     </nav>
