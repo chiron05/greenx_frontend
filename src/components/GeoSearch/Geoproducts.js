7e833cd8cc88564e6ac323f24dfbc76e4bb4eb7a
sarthak <sarthaknaik010@gmail.com>
2023-07-17 11:02:49 +0530

    resposnive geosearch

--- src/components/GeoSearch/Geoproducts.js
+++ src/components/GeoSearch/Geoproducts.js
@@ -1,23 +1,25 @@
-import React from 'react'
+import React from 'react';
 import Geomap from './css/Geoproduct.css';
 import Card from '../Card/Card';
+
 const Geoproducts = (props) => {
   const { products } = props;
+
   return (
     <div className='geomain-container'>
-    {
-      products.map((product)=>{
-        return <>
-        <div className='geocontainer-card'>
-        <Card  id={product._id} name={product.name} description={product.description} image={product.images[0]} price={product.price} ></Card>
+      {products.map((product, index) => (
+        <div key={index} className='geocontainer-card'>
+          <Card
+            id={product._id}
+            name={product.name}
+            description={product.description}
+            image={product.images[0]}
+            price={product.price}
+          />
         </div>
-        </>
-      })
-    }
-     
+      ))}
     </div>
+  );
+};
 
-  )
-}
-
-export default Geoproducts
+export default Geoproducts;
