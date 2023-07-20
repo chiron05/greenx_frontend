dc224835e202275eb9c8c7c20ad04de9c42d2a14
Praharsh-pai <dementor1901@gmail.com>
2023-07-20 22:34:10 +0530

    add

--- src/components/SellDashboard.js
+++ src/components/SellDashboard.js
@@ -75,28 +75,43 @@ const SellDashboard = () => {
   return (
     <>
       <NavbarDasBoard />
-      <section style={{ display: "flex", flexDirection: "row", justifyContent: "space-around", alignItems: "center", paddingTop: "150px", paddingBottom: "150px", paddingRight: "250px" }}>
+      <section style={{ display: "flex", flexDirection: "row", justifyContent: "space-around", alignItems: "center", paddingTop: "100px", paddingBottom: "100px", paddingRight: "250px" }}>
         <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-around", alignItems: "center" }}>
           <div className="container-main">
-            <div className="description" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <div className="description" style={{ display: "flex", flexDirection: "column", alignItems: "center", width: "300px", height: "300px" }}>
               <div className="welcome-container" style={{ textAlign: "center" }}>
                 <h1 className='titlehead'>HELLO {userName}</h1>
                 <h1 className='titlehead'>Welcome to GREEN<span style={{ color: "#ff00006d" }}>X</span>!</h1>
               </div>
             </div>
-            <div className="circle">
+            <div className="circle" style={{ width: "500px", height: "500px" }}>
               <img src="https://static.vecteezy.com/system/resources/previews/017/345/884/original/go-green-text-sign-with-hand-planting-tree-environment-protection-and-save-world-free-png.png" alt="Description of" />
             </div>
           </div>
         </div>
       </section>
 
-      <section style={{ display: "block", paddingTop: "250px"}}>
-        <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-around", alignItems: "center" }}>
-          <button className='addBtn'> Pick up!</button>
-          <button className='addBtn'> Pick up!</button>
-          <button className='addBtn'> Pick up!</button>
-          <button className='addBtn'> Pick up!</button>
+
+
+      <section style={{ display: "flex", paddingTop: "250px", flexDirection: "column", justifyContent: "space-around", alignItems: "center" }}>
+        <h1 style={{ textAlign: 'center', color: '#92c394', marginTop: "60px", marginBottom: "60px" }}>Inventory management</h1>
+        <div className="btnBack" style={{ display: "flex", flexDirection: "row", justifyContent: "space-around", alignItems: "center", width:"80%"}}>
+          <div  style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", width: "50%", height: "60vh"}}>
+            <button class="Addbtn" style={{margin:"20px 30px"}}>
+            ADD PRODUCT
+            </button>
+            <button class="Addbtn" style={{margin:"20px 30px"}}>
+            EDIT PRODUCT
+            </button>
+            <button class="Addbtn" style={{margin:"20px 30px"}}>
+            DELETE PRODUCT
+            </button>
+          </div>
+          <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", width: "50%", height: "60vh" }}>
+            <div style={{ width: "80%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center", overflow: "hidden" }}>
+              <img src="https://www.kindpng.com/picc/m/770-7708589_certificate-in-inventory-management-inventory-management-system-png.png" alt="Description of" />
+            </div>
+          </div>
         </div>
       </section>
 
